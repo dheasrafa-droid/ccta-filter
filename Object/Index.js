9ab46e8ef98aa5f0db0f878d2c241c55d588c1e7
dheasrafa-droid <dheasrafa@gmail.com>
2025-09-19 22:34:02 +0700

    Create Index.js

---
+++ Object/Index.js
@@ -0,0 +1,10 @@
+export { Cube } from "./Cube.js";
+export { Sphere } from "./Sphere.js";
+export { Plane } from "./Plane.js";
+export { Cone } from "./Cone.js";
+export { Cylinder } from "./Cylinder.js";
+export { Torus } from "./Torus.js";
+export { TorusKnot } from "./TorusKnot.js";
+export * from "./Platonic/index.js";
+export * from "./Derived/index.js";
+export * from "./Misc/index.js";
