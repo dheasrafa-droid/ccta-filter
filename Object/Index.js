6ff253b3dc346b3eff8ab1642b9d4419fe7d13a5
dheasrafa-droid <dheasrafa@gmail.com>
2025-09-19 23:18:04 +0700

    Update Index.js

--- Object/Index.js
+++ Object/Index.js
@@ -5,6 +5,7 @@ export { Cone } from "./Cone.js";
 export { Cylinder } from "./Cylinder.js";
 export { Torus } from "./Torus.js";
 export { TorusKnot } from "./TorusKnot.js";
+
 export * from "./Platonic/index.js";
 export * from "./Derived/index.js";
 export * from "./Misc/index.js";
