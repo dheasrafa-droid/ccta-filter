1be7d807b61f382ca044aa18e73c68c6f256b8a7
dheasrafa-droid <dheasrafa@gmail.com>
2025-09-19 21:57:14 +0700

    Create index.js

---
+++ color/index.js
@@ -0,0 +1,26 @@
+// Warna dasar
+export { Gray } from "./Gray.js";
+export { Red } from "./Red.js";
+export { Blue } from "./Blue.js";
+export { Green } from "./Green.js";
+export { Yellow } from "./Yellow.js";
+export { Purple } from "./Purple.js";
+export { Orange } from "./Orange.js";
+export { Pink } from "./Pink.js";
+export { Teal } from "./Teal.js";
+export { Indigo } from "./Indigo.js";
+export { Cyan } from "./Cyan.js";
+export { Sky } from "./Sky.js";
+export { LightBlue } from "./LightBlue.js";
+export { Amber } from "./Amber.js";
+export { Emerald } from "./Emerald.js";
+export { Rose } from "./Rose.js";
+export { Fuchsia } from "./Fuchsia.js";
+export { Violet } from "./Violet.js";
+export { Lime } from "./Lime.js";
+
+// Warna netral / abu
+export { Stone } from "./Stone.js";
+export { Neutral } from "./Neutral.js";
+export { Zinc } from "./Zinc.js";
+export { Slate } from "./Slate.js";
