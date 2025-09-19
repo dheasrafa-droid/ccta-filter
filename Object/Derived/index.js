da2ef5a59bb05642466f32ca6d0931ab1d2a1ac0
dheasrafa-droid <dheasrafa@gmail.com>
2025-09-19 23:14:44 +0700

    Create index.js

---
+++ Object/Derived/index.js
@@ -0,0 +1,4 @@
+export { Pyraminx } from "./Pyraminx.js";
+export { SkewbUltimate } from "./SkewbUltimate.js";
+export { SkewbExtreme } from "./SkewbExtreme.js";
+export { HollowSkewbUltimate } from "./HollowSkewbUltimate.js";
