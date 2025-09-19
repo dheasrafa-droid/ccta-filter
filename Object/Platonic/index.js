860d66e01d4ad011a56e18ec74a0c04fb5c1b66a
dheasrafa-droid <dheasrafa@gmail.com>
2025-09-19 22:55:39 +0700

    Create index.js

---
+++ Object/Platonic/index.js
@@ -0,0 +1,4 @@
+export { Tetrahedron } from "./Tetrahedron.js";
+export { Octahedron } from "./Octahedron.js";
+export { Dodecahedron } from "./Dodecahedron.js";
+export { Icosahedron } from "./Icosahedron.js";
